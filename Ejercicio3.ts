583cd0a892557142d99c71295d9db760a9a95f3d
Leonardo <leonardo38361@gmail.com>
2023-10-22 19:39:39 -0300

    ejercicio3

---
+++ Ejercicio3.ts
@@ -0,0 +1,13 @@
+//TypeScript sirve para agregar tipado y otras características orientadas a la programación orientada a objetos
+
+// Declaración de una variable con tipo
+let numero: number = 42;
+
+// Declaración de una función con tipos
+function saludar(nombre: string): string {
+  return `Hola, ${nombre}!`;
+}
+
+// Uso de la variable y la función
+console.log(numero);
+console.log(saludar("Juan"));
